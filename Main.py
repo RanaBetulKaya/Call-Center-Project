3879901c96f8d63f6d089360e6a0757800ba2b01
RanaBetulKaya <rana20032016@icloud.com>
2024-12-03 23:10:39 +0300

    file name&content taken with ray

--- Main.py
+++ Main.py
@@ -4,31 +4,42 @@ from AudioProcessor import AudioProcessor
 from OutputWriter import OutputWriter
 
 def get_audio_files(directory):
-    if not os.path.exists(directory):
-        print(f"Directory {directory} does not exist. Skipping audio file processing.")
-        return []
+    print("buradayım")
     return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.wav')]
 
+@ray.remote
+def process_audio_file(audio_file):
+    print(f"Running on node: {ray.util.get_node_ip_address()}")
+    processor = AudioProcessor(audio_file)
+    processor.load_models()
+    return processor.process_audio()
+
+
+
 def main():
-    audio_directory = "/Users/cansuoksel/Downloads/Call-Center-Project/data"
+    ray.init()
+    print(ray.nodes())
+    # Dosya dizini değiştirilmeli
+    audio_directory = "/app/data"
+    
+    # Dizindeki tüm .wav dosyalarını al
     audio_files = get_audio_files(audio_directory)
+
+    # Eğer dizinde ses dosyası yoksa bir mesaj yazdır
     if not audio_files:
-        print("No audio files found. Exiting.")
+        print("Ses dosyası bulunamadı.")
         return
 
-    ray.init()
-
-    futures = []
-    processor = AudioProcessor()
-    output_writer = OutputWriter()
-
-    for audio_file in audio_files:
-        futures.append(ray.remote(processor.transcribe).remote(audio_file))
-
+    futures = [process_audio_file.remote(audio_file) for audio_file in audio_files]
+        
     while len(futures):
         done_id, futures = ray.wait(futures)
-        file_name, transcription = os.path.basename(ray.get(done_id[0])), ray.get(done_id[0])
-        output_writer.write_output(file_name, transcription)
+        # file_name, transcription = os.path.basename(ray.get(done_id[0])), ray.get(done_id[0])
+        # output_writer.write_output(file_name, transcription)
+        outputWriter = OutputWriter()
+        print(ray.get(done_id[0])[0])
+        print(ray.get(done_id[0])[1])
+        outputWriter.write_output(ray.get(done_id[0])[0], ray.get(done_id[0])[1])
 
 if __name__ == "__main__":
     main()
