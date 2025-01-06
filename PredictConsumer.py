95c7bc8d4c2595d2ca098516c0231690486ca3c4
RanaBetulKaya <rana20032016@icloud.com>
2025-01-06 14:45:01 +0300

    Consumer code has been changed

--- PredictConsumer.py
+++ PredictConsumer.py
@@ -1,39 +1,41 @@
-import threading
-from confluent_kafka import Consumer
-from DialogFormat import dialog_format
+from confluent_kafka import Consumer, KafkaException, KafkaError
 import json
-
-dialogs = {}
-class PredictConsumer:
-    def __init__(self, topic="topics", bootstrap_servers="localhost:9092", group_id="group1"):
-        self.consumer = Consumer({
-            'bootstrap.servers': bootstrap_servers,
-            'group.id': group_id,
-            'auto.offset.reset': 'earliest'
-        })
-        self.topic = topic
-        self.consumer.subscribe([self.topic])
-    def read_messages(self):
-        print(f"Listening to Kafka topic '{self.topic}'...")
-        try:
-            while True:
-                msg = self.consumer.poll(1.0)
-                if msg is None:
-                    continue
-                if msg.error():
-                    print(f"Consumer error: {msg.error()}")
-                    continue
-                message = msg.value().decode('utf-8')
-                json_data = json.loads(message) 
-                print(f"Received message")
-                formatted_dialog = dialog_format(json_data)
-                print(formatted_dialog)
-        except KeyboardInterrupt:
-            print("Stopped by user")
-    
-    def start(self):
-        consumer_thread = threading.Thread(target=self.read_messages)
-        consumer_thread.daemon = True  
-        consumer_thread.start()
-
-    
+ 
+ 
+conf = {
+    'bootstrap.servers': 'localhost:9092',  
+    'group.id': 'grup3',                
+    'auto.offset.reset': 'earliest'        
+}
+ 
+# Kafka Consumer oluşturma
+consumer = Consumer(conf)
+ 
+# Mesajları almak istediğiniz Kafka topic ismi
+topic = 'topics'
+ 
+consumer.subscribe([topic])
+ 
+def read_messages():
+    try:
+        while True:
+            msg = consumer.poll(timeout=5.0)
+           
+            if msg is None:
+                continue  
+           
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                   
+                    print(f"End of partition {msg.partition} reached {msg.offset}")
+                else:
+                    raise KafkaException(msg.error())
+            else:
+                message = json.loads(msg.value().decode('utf-8'))
+                yield message  
+               
+    except KeyboardInterrupt:
+        print("Consumer stopped")
+ 
+    finally:
+        consumer.close()  
